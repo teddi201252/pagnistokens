9ed1afc73de94a491cbe55558344020e120bc043
Mikael Pro <teddi201252@gmail.com>
2021-05-12 00:23:41 +0200

    Creato database mysql e connessione

--- PagnisTokens/Views/TabbedMain.xaml.cs
+++ PagnisTokens/Views/TabbedMain.xaml.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using MySqlConnector;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PagnisTokens.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+	[XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TabbedMain : TabbedPage
     {
         public TabbedMain()
         {
             InitializeComponent();
+
+            MySqlConnection connection = new MySqlConnection("Server=remotemysql.com;Port=3306;Database=7ZZfKCRq3R;Uid=7ZZfKCRq3R;Pwd=KIPGq6QV7W;");
+            connection.Open();
         }
     }
 }
